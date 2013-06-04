d38fd93d55105b2e5ba87f0fbca31b7132163f98
Lynx3d <lynx3d@some-imaginary-isp.org>
2013-06-04 18:17:28 +0200

    Add two more UI classes
    Thanks to Pengoros for allowing me to use his code.

--- LynxPlugins/Utils/__init__.lua
+++ LynxPlugins/Utils/__init__.lua
@@ -2,3 +2,5 @@ import "LynxPlugins.Utils.Class";
 import "LynxPlugins.Utils.Type";
 import "LynxPlugins.Utils.Misc";
 import "LynxPlugins.Utils.NumericInput";
+import "LynxPlugins.Utils.Slider";
+import "LynxPlugins.Utils.ComboBox";
